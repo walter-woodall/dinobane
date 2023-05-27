7fc2560c678e51cee7024c62df54399cd248ccd9
Walter Woodall <walter.woodall.iv@gmail.com>
2023-05-27 11:17:52 -0700

    Implement basic map, player and moving functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+mod map;
+mod player;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+    pub use crate::player::*;
+}
+
+use prelude::*;
+struct State {
+    map: Map,
+    player: Player,
+}
+
+impl State {
+    fn new() -> Self {
+        let starting_point = Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+        State {
+            map: Map::new(),
+            player: Player::new(starting_point),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        self.player.update(ctx, &self.map);
+        self.map.render(ctx);
+        self.player.render(ctx);
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("DinoBane 4: Barbarian Carnage")
+        .with_fps_cap(60.0)
+        .build()?;
+    main_loop(context, State::new())
 }
---
+++ src/player.rs
@@ -0,0 +1,38 @@
+use crate::prelude::*;
+
+pub struct Player {
+    pub position: Point,
+}
+
+impl Player {
+    pub fn new(position: Point) -> Self {
+        Player { position }
+    }
+
+    pub fn update(&mut self, ctx: &mut BTerm, map: &Map) {
+        if let Some(key) = ctx.key {
+            let delta = match key {
+                VirtualKeyCode::W => Point::new(0, -1),
+                VirtualKeyCode::A => Point::new(-1, 0),
+                VirtualKeyCode::S => Point::new(0, 1),
+                VirtualKeyCode::D => Point::new(1, 0),
+                _ => Point::zero(),
+            };
+
+            let new_position = self.position + delta;
+            if map.can_enter_tile(new_position) {
+                self.position = new_position;
+            }
+        }
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        ctx.set(
+            self.position.x,
+            self.position.y,
+            WHITE,
+            BLACK,
+            to_cp437('@'),
+        )
+    }
+}
